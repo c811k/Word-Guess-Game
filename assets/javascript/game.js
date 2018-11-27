e52e4652b920c6a9a544ce92cec155ef5f1e0a80
c811k <bhk811@gmail.com>
2018-11-26 17:54:16 -0800

    javascript complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,48 +10,67 @@ var guesses = 12;
 
 // Randomly chooses a choice from the options array. This is the Computer's guess.
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-console.log(computerGuess);
 
 for(i = 0; i <computerGuess.length; i++) {
     userAnswer[i]= "_";
 }
+document.getElementById("answer").textContent = userAnswer.join(" ");
 
 //This function is run whenever the user presses a key.
 document.onkeyup = function(event) {
     
+    //
     var userGuess = event.key;
-
     if(userChoices.includes(userGuess)) {
         return;
     }
     else {
         userChoices.push(userGuess);
+        guesses--;
     }
     
-
+    //
     for(i = 0; i < computerGuess.length; i++) {
         if(computerGuess[i] === userGuess) {
             userAnswer[i] = userGuess;
         }        
     }
-    if(userGuess !== computerGuess) {
-        guesses--;
-    }
-    
 
-    console.log("User Choices: " + userChoices);
-    // var letter = computerGuess.search(userGuess);
+    //
+    if(guesses === 0) {
+        losses++;
+        guesses = 12;
+        userAnswer = [];
+        userChoices = [];
+        computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+        for(i = 0; i <computerGuess.length; i++) {
+            userAnswer[i]= "_";
+        }
     
-    // if(letter >= 0) {
-    //     userAnswer[letter] = userGuess;
-    // }
-   
+    }
+    else if (userAnswer.includes("_") === false) {
+        wins++;
+        guesses = 12;
+        userAnswer = [];
+        userChoices = [];
+        computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+        for(i = 0; i <computerGuess.length; i++) {
+        userAnswer[i]= "_";
+        }
+
+    }
+    document.getElementById("wins").textContent = wins;
+    document.getElementById("user-choices").textContent = userChoices;
+    document.getElementById("guesses").textContent = guesses;
+    document.getElementById("answer").textContent = userAnswer.join(" ");
+
     console.log("Wins: " + wins);
     console.log("Losses: " + losses);
     console.log("Answer: " + computerGuess);
     console.log("What you have so far: " + userChoices);
     console.log("Guesses left: " + guesses);
-    userAnswer.join('');
-    console.log("User Answer: " + userAnswer.join(" "));
+    console.log(userAnswer.join(" "));
 
 };
